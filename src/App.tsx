59dc28c91f72ac3292069e219ec015e1c591c8e9
Alex <grok88@tut.by>
2020-06-04 07:43:34 +0300

    HZ

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
-import Display from "./components/display/Display";
-import ButtonPanel from "./components/button-panel/ButtonPanel";
+import {Counter} from "./Counter";
+import {v1} from "uuid";
 
 
 function App() {
@@ -16,16 +16,47 @@ function App() {
         setCounter(0);
     }
 
+    let [counters, setCounters] = useState([
+        {
+            id: v1(),
+            // buttons:[
+            //     {buttonId:v1(), title:"INC", disabled:counter === maxCount},
+            //     {buttonId:v1(), title:"RESET", disabled:counter === 0},
+            // ]
+        }
+    ]);
+
     return (
-        <div className={'counterBlock'}>
-            <Display counter={counter} maxCount={maxCount}/>
-            <ButtonPanel incrementCounter={incrementCounter}
-                         resetCounter={resetCounter}
-                         counter={counter}
-                         maxCount={maxCount}
-            />
-        </div>
+        <>
+            {
+                counters.map(elem => {
+                    return <Counter
+                        key={elem.id}
+                        resetCounter={resetCounter}
+                        incrementCounter={incrementCounter}
+                        maxCount={maxCount}
+                        counter={counter}
+                      //  buttons={elem.buttons}
+                    />
+                })
+            }
+            {/*<Counter resetCounter={resetCounter}*/}
+            {/*         incrementCounter={incrementCounter}*/}
+            {/*         maxCount={maxCount}*/}
+            {/*         counter={counter}*/}
+            {/*/>*/}
+        </>
     );
+
+    /* <div className={'counterBlock'}>
+         <Display counter={counter} maxCount={maxCount}/>
+         <ButtonPanel incrementCounter={incrementCounter}
+                      resetCounter={resetCounter}
+                      counter={counter}
+                      maxCount={maxCount}
+         />
+     </div>*/
 }
 
 export default App;
+
---
+++ src/components/button-panel/Button.tsx
@@ -0,0 +1,20 @@
+import style from "./ButtonPanel.module.css";
+import React from "react";
+
+type ButtonType = {
+    typeCounter: () => void;
+    title: string,
+    disabled: boolean
+}
+
+export const Button = (props: ButtonType) => {
+    debugger
+    const {typeCounter, title, disabled} = props;
+    return (
+        <button className={style.incButton}
+                onClick={typeCounter}
+                disabled={disabled}
+        >{title}
+        </button>
+    );
+}
